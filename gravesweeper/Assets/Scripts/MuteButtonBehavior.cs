a48b2246a30c450b7d38c75f32e144915f89ab8b
lpasquini24 <123428916+lpasquini24@users.noreply.github.com>
2024-12-02 18:31:24 -0500

    Added UI art

--- gravesweeper/Assets/Scripts/MuteButtonBehavior.cs
+++ gravesweeper/Assets/Scripts/MuteButtonBehavior.cs
@@ -4,20 +4,22 @@ using UnityEngine.UI;
 public class MuteButtonBehavior : MonoBehaviour
 {
     public bool muted = false;
-    public Sprite unmutedSprite;
-    public Sprite mutedSprite;
+    public GameObject unmutedSprite;
+    public GameObject mutedSprite;
     public void OnButtonPressed()
     {
         if (muted)
         {
             muted = false;
-            GetComponent<Image>().sprite = unmutedSprite;
+            unmutedSprite.SetActive(true);
+            mutedSprite.SetActive(false);
             AudioManager.SetMasterVolume(1);
         }
         else
         {
             muted = true;
-            GetComponent<Image>().sprite = mutedSprite;
+            mutedSprite.SetActive(true);
+            unmutedSprite.SetActive(false);
             AudioManager.SetMasterVolume(0);
         }
     }
